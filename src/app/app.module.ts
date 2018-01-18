63426db2e4e1bf1c2c9a25b6df2554708633ac84
Nicholas Arthur <nicolas.arthur_ext@lpsolutions.com>
2018-01-18 16:10:01 +0100

    Demonstrate sourcemap bug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { MaterialModule } from './MaterialModule';
 import { AppComponent } from './app.component';
 
 
@@ -10,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
